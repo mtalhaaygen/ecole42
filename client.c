077ecd77bfdcf14feae4777b8a502dfb5e528abb
Muhammed Talha Aygen <maygen@k1m23s02.42istanbul.com.tr>
2022-12-30 18:13:30 +0300

    son kontroller

--- client.c
+++ client.c
@@ -6,24 +6,23 @@
 /*   By: maygen <maygen@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 18:01:47 by maygen            #+#    #+#             */
-/*   Updated: 2022/12/20 18:04:31 by maygen           ###   ########.fr       */
+/*   Updated: 2022/12/30 16:59:56 by maygen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h> // usleep getpid
+#include <unistd.h>
 #include <signal.h>
-#include <stdlib.h> // atoi için
 
 int	ft_atoi(const char *str)
 {
-	int i;
-	int neg;
-	int num;
+	int	i;
+	int	neg;
+	int	num;
 
 	i = 0;
 	num = 0;
 	neg = 1;
-	while ((str[i] <= 9 && str[i] >= 13) || str[i] == 32)
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
 		i++;
 	if (str[i] == '-')
 	{
@@ -40,29 +39,29 @@ int	ft_atoi(const char *str)
 	return (neg * num);
 }
 
-int main(int ac, char **av) // ./a.out 41133 "talha aygen"
+int	main(int ac, char **av)
 {
-    if (ac == 3)
-    {
-        int i;
-        int j;
-        pid_t serverpid;
+	pid_t	serverpid;
+	int		j;
+	int		i;
 
-        i = 0;
-        serverpid = ft_atoi(av[1]);
-        while (av[2][i])
-        {
-            j = 0;
-            while (j < 8)
-            {
-                if((av[2][i] >> j) & 1)
-                    kill(serverpid, SIGUSR1);
-                else
-                    kill(serverpid, SIGUSR2);
-                usleep(100);
-                j++;
-            }
-            i++;
-        }
-    }
+	if (ac == 3)
+	{
+		i = 0;
+		serverpid = ft_atoi(av[1]);
+		while (av[2][i])
+		{
+			j = 0;
+			while (j < 8)
+			{
+				if ((av[2][i] >> j) & 1)
+					kill(serverpid, SIGUSR1);
+				else
+					kill(serverpid, SIGUSR2);
+				usleep(100);
+				j++;
+			}
+			i++;
+		}
+	}
 }
--- server.c
+++ server.c
@@ -6,18 +6,18 @@
 /*   By: maygen <maygen@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 18:01:53 by maygen            #+#    #+#             */
-/*   Updated: 2022/12/20 18:20:02 by maygen           ###   ########.fr       */
+/*   Updated: 2022/12/30 17:03:23 by maygen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h> // usleep getpid
+#include <unistd.h>
 #include <signal.h>
 
-void ft_putnbr(int nb)
+void	ft_putnbr(int nb)
 {
-    if (nb < 10)
+	if (nb < 10)
 	{
-        nb += 48;
+		nb += 48;
 		write(1, &nb, 1);
 	}
 	else
@@ -27,43 +27,45 @@ void ft_putnbr(int nb)
 	}
 }
 
-static void myhandler(int signum)
+static void	myhandler(int signum)
 {
-    char new;
-    static int i;
-    char gelenk;
-    gelenk = 0;
-    if (signum == 30)
-    {
-        gelenk |= (1 << i);
-        new += gelenk;
-    }
-    if (i == 7)
-    {
-        write(1, &new, 1);
-        i = 0;
-        new = 0;
-        return ;
-    }
-    i++;
+	static char	new;
+	static int	i;
+	char		gelenk;
+
+	gelenk = 0;
+	if (signum == 30)
+	{
+		gelenk |= (1 << i);
+		new |= gelenk;
+	}
+	if (i == 7)
+	{
+		write(1, &new, 1);
+		i = 0;
+		new = 0;
+		return ;
+	}
+	i++;
 }
 
-int main()
+int	main(void)
 {
-    pid_t pid = getpid();
-    struct sigaction sa;
-    
-    sa.sa_handler = myhandler;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = SA_SIGINFO;
-    if (sigaction(SIGUSR1, &sa, NULL) == -1)
-        write(1, "hatalı dönüş değeri", 25);
+	struct sigaction	sa;
+	pid_t				pid;
 
-    signal(SIGUSR2, myhandler);
-
-    write(1,"pid : ", 7);
-    ft_putnbr(pid);
-    write(1, "\n", 1);
-    while (1);
-    return (0);
+	pid = getpid();
+	sa.sa_handler = myhandler;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_SIGINFO;
+	if (sigaction(SIGUSR1, &sa, NULL) == -1)
+		write(1, "hatalı dönüş değeri", 25);
+	signal(SIGUSR2, myhandler);
+	write(1, "pid : ", 7);
+	ft_putnbr(pid);
+	write(1, "\n", 1);
+	while (1)
+	{
+	}
+	return (0);
 }
